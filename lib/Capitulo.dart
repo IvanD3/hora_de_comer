1ab1390dcca3ba722ad768f15b6d911d849aeb32
Ivan <ivanpsd3@gmail.com>
2020-03-28 02:02:35 +0100

    Carga de datos de una Google SpreadSheet Función de abrir URL aleatoria de capítulos

---
+++ lib/Capitulo.dart
@@ -0,0 +1,7 @@
+class Capitulo{
+  int temporada, episodio;
+  String url;
+  Capitulo( this.temporada,
+            this.episodio,
+            this.url);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,28 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'Capitulo.dart';
+import 'package:gsheets/gsheets.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'dart:async';
+
+const _credentials = r'''
+{
+  "type": "service_account",
+  "project_id": "hdc-ls",
+  "private_key_id": "76e349d6a2a599d903d483e24825343790ff568b",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDKtz2QIfGLnytN\n28SIyW+QcaTW4id0Yh+TorZZsFRa6sPmTa9XpkvYL06HQZfSLi/lRqVJE2hkY/fP\nMVXDJrG3nK6jz2wUI6OQPv5kVbZZwXrrcAK7boq8uFcTXznpZ3mnWlxs+u6SR6YQ\n25yDw2s0qFJ731sksj8t7PHQ6OSVWx2QDvWZ5ovCzb+7dZOzOSNUdmz5KBVS4gjj\nHrLhh/0HRrWP8seimMikXgk7goI9ArFl00VwJFNnoX9tnzWkNhnkqO7tBQHN3dnq\nU4sjXa5a5AJXD1kLbgTGdg4jqwpJZOcRqO3ZbPXO9LzoFWOqj5ErjzOmVA3o1JdI\nWs5KNkZXAgMBAAECggEAG02g6hfYS8DteTgfeXkAIMldtV+Shtdsf1sMCnp4ciJq\nrktKUZjhYEXG4urPwwxbgRsJyhTmG5dnWi2/6l/80ck1sHSQjwnZzuTdKLButhoc\nLzfP9mTGJPMDL2XFDA3W0daW2krV9TfXQzABHbIlb1s4eNy1jvS/E7V1QHkNyJvW\nkqSkQADlRizSqksp2mIo/Yo5sb43q1HlR764mAl7VL3RbMSCwD1lWBmIPOFwfpwB\nQ1vQDqts3HOrh08KmGAS6e4ZZ8T5AP5j29A27RDXdWj0ZanRf7vLzYVEEhPoU7Y3\nThFpQZim+aYp6yI6LVi203rjz5WsjtvrvudZkkA7CQKBgQD0sY88BUdCzNiJ75OV\nxfyu1OOoBKcVSZiyshCvJj87p5ieDDOTPHQBetH41cRGDenF1ZAAon/IwkZ4azzV\nREmBSXE7O861wP90i7hf056WPgfzAJUaj3i0QWGrEwpYnqYEZfQ0VxIQiAvIiLlQ\n0QNCFacLabDnZ8yKgLcI9WDobQKBgQDUFSHgtuX0Y5x/dTrg6K62ObOQUUibwtMM\npzVjrU2WiAvhFwyuy1srLPGl4/CZviY8WjNzXcwdI2HuLyz8mJMWeXzrd3y/07HL\nmG8Ql7T8ft/HGua8TrxW1USZm4wti5TyusQk7p81I7oDVDCPzW2tignvnIDsiu+r\ntyny7xm3UwKBgCE9qc6mjOq3N0dtw2faUJxTkWW21BfhOpwz8m5IzNhGyUXXNH5w\n/PVigPnD2HkH1NUqUPi1GpTkR3x+XPI/55hnqLk6Q7ePM2Tawj5KWkXgwWWyk47b\nJLOgGiuKV6J4UudQDl+54Ftcj/U7Famz6zRkCqBTSFP3VuMN30+ZPgP1AoGAWfJY\neui3IG46ymfav9aPeqOAP33p0H3RarVj1FGE7Ynnl+BhtaRzlx0po5THzzyxFLmg\nxpYKYnVGLHF/n+XJxCdkTq1PkD/lURbmS+A7lzbsC5KiN00hXaZ3wLoSuqJPWp7R\nX2SAy1XKp2zGZ8bVdFtuGVyD5U6LlYVKWkkD3BcCgYBQnwTe4P3PjOTgZ3cwDkrJ\n2bGoWjJtpr7oJ/gPCRhcTLucUK0Er0tL0goNQUV8ghdzM35Awgp9TOzEklMVVVhm\npm0Rf5+Hhlmsla/vrPUCxyvPFsbyLDdUsrlwUR8fo85MsMvLCoLCxwCo/iTqaEWM\n9P5d6QksmIaIWLed5Yk4hA==\n-----END PRIVATE KEY-----\n",
+  "client_email": "gsheets@hdc-ls.iam.gserviceaccount.com",
+  "client_id": "102683251242105420331",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/gsheets%40hdc-ls.iam.gserviceaccount.com"
+}
+''';
+
+const _spreadsheetId = '1gQ0jNI9jXdjW2Jq52OtFBDAaqhZyg9pdBLHHgYFB-hQ';
+
 
 void main() => runApp(MyApp());
 
@@ -45,7 +69,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
+  List<String> enlacesCapitulos = new List<String>();
+  //List<Capitulo> listaEpisodios = new List<Capitulo>();
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -56,20 +81,47 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
+  void _peticionHojaCalculo() async{
+    print("Vamos al putiferio");
+    final gsheets = GSheets(_credentials);
+    final ss = await gsheets.spreadsheet(_spreadsheetId);
+    var sheet = await ss.worksheetByTitle('capitulazos');
+    //await sheet.values.insertValue('PUTAAA', column: 5, row: 2);
+    var celdas = await sheet.cells.findByValue("1");
+    List<String> direcciones = await sheet.values.column(3);
+    print(direcciones);
+    print(direcciones.runtimeType);
+    print(direcciones[0]);
+    print(direcciones[0].runtimeType);
+  }
 
+  _launchURL(url) async {
+
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+  Future<List<String>> _cargarURLCapitulos()async{
+    final gsheets = GSheets(_credentials);
+    final ss = await gsheets.spreadsheet(_spreadsheetId);
+    var sheet = await ss.worksheetByTitle('capitulazos');
+    List<String> direcciones = await sheet.values.column(3);
+    Random rng = new Random();
+    print(rng.nextInt(direcciones.length-1));
+    _launchURL(direcciones[rng.nextInt(direcciones.length-1)]);
+    return direcciones;
+  }
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
+    //listaEpisodios.add(new Capitulo(1,1,'https://www.disneyplus.com/es-es/video/79529cd0-f1cf-4eec-8b1d-ea1e1b76043b'));
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Putaa"),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -102,10 +154,11 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _cargarURLCapitulos,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
